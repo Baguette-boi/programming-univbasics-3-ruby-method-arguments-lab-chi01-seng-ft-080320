483a31ed6003cfc951c0fe9c26b7939d77555f2c
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-23 04:02:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ introduction("Victor")
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language(Juan, Ruby)
+introduction_with_language
 
 
 def introduction_with_language_optional(name, language = "Ruby")
